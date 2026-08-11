755ad12531b03f8d532d6b471cffc3a0798809a3
chetanareddy21 <chetanareddy12345@gmail.com>
2026-08-11 23:14:27 +0530

    added test prop

--- src/test/java/org/xyonsoft/bankingapp/BankingAppApplicationTests.java
+++ src/test/java/org/xyonsoft/bankingapp/BankingAppApplicationTests.java
@@ -2,12 +2,13 @@ package org.xyonsoft.bankingapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BankingAppApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
